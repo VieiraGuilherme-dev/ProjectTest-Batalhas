7f7f4f3807731c1daef2787d78e33f4ce68809ca
VieiraGuilherme-dev <gv524003@gmail.com>
2025-10-01 16:53:35 -0300

    Execute attack

--- src/Service/BatalhaService.java
+++ src/Service/BatalhaService.java
@@ -1,7 +1,11 @@
 package Service;
 
+import model.Criatura;
+import model.EfeitoStatus;
+import model.Habilidade;
+
 public class BatalhaService {
-    public static void iniciarBatalha(Criatura c1, Criatura c2, Habilidade habilidade){
+    public static void iniciarBatalha(Criatura atacante, Criatura defensor, Habilidade habilidade){
         if(atacante.estaDerrotado()){
             System.out.println(atacante.getNome() + "esta derrotado.");
             return;
@@ -11,6 +15,36 @@ public class BatalhaService {
             return;
         }
 
+        double multiplicador = CalculadoraElemental.calcularMultiplicador(habilidade.getTipo(), defensor.getTipo());
 
+        int danoBase = atacante.getAtk() + habilidade.getPoder() - defensor.getDef();
+        if(danoBase < 0) danoBase = 0;
+        int danoFinal = (int) Math.round(danoBase * multiplicador);
+
+        int novoHp =  defensor.getHp() - danoFinal;
+        defensor.setHp(novoHp);
+        System.out.println(atacante.getNome() + "usou" + habilidade.getNome() + "causando" + danoFinal + "de dano em" + defensor.getNome() + ".");
+
+        if(multiplicador > 1.0){
+            System.out.println("É muito bom!");
+        }else if(multiplicador < 1.0){
+            System.out.println("Não é tão bom!");
+        }
+        if(defensor.estaDerrotado()){
+            System.out.println("Defensor" + defensor.getNome() + "está derrotado!");
+        }
+
+        EfeitoStatus efeito = habilidade.getEfeito();
+        if(efeito != null && efeito != EfeitoStatus.CURADO){
+            defensor.aplicarEfeito(efeito);
+            System.out.println(defensor.getNome() + "foi atingido por " + efeito);
+        }
+
+        if(efeito == EfeitoStatus.CURADO){
+            int cura = habilidade.getPoder();
+            int hpCurado = atacante.getHp() + cura;
+            atacante.setHp(hpCurado);
+            System.out.println(atacante.getNome() + "restaurou" + cura + "de HP.");
+        }
     }
 }
