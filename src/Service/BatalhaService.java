3f07a16539cd214269c31fccbe6ac72196307f93
VieiraGuilherme-dev <gv524003@gmail.com>
2025-10-01 12:57:42 -0300

    Service e ElementalCalculator created

--- src/Service/BatalhaService.java
+++ src/Service/BatalhaService.java
@@ -1,4 +1,16 @@
 package Service;
 
 public class BatalhaService {
+    public static void iniciarBatalha(Criatura c1, Criatura c2, Habilidade habilidade){
+        if(atacante.estaDerrotado()){
+            System.out.println(atacante.getNome() + "esta derrotado.");
+            return;
+        }
+        if(defensor.estaDerrotado()){
+            System.out.println(defensor.getNome() + "já esta derrotado");
+            return;
+        }
+
+
+    }
 }
