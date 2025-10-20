6ffe1cb364a5c0767b8225257cf3fa90becdf1f9
VieiraGuilherme-dev <gv524003@gmail.com>
2025-10-20 20:36:12 -0300

    class item atualizada

--- src/main/java/model/Item.java
+++ src/main/java/model/Item.java
@@ -1,4 +1,33 @@
 package model;
 
 public class Item {
+    private String nome;
+    private String decricao;
+    private TipoItem tipo;
+    private int valor;
+
+    public Item(String nome, String decricao, TipoItem tipo, int valor) {
+        super();
+        this.nome = nome;
+        this.decricao = decricao;
+        this.tipo = tipo;
+        this.valor = valor;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getDecricao() {
+        return decricao;
+    }
+
+    public TipoItem getTipo() {
+        return tipo;
+    }
+
+    public int getValor() {
+        return valor;
+    }
 }
+
