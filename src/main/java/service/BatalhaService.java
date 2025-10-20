f0f0bb2d6dd17d8e408496d4e12460617b848d08
VieiraGuilherme-dev <gv524003@gmail.com>
2025-10-20 20:41:38 -0300

    BatalhaService atualizada com iniciarBatalha

--- src/main/java/service/BatalhaService.java
+++ src/main/java/service/BatalhaService.java
@@ -1,4 +1,13 @@
 package service;
 
 public class BatalhaService {
-}
+    public static void iniciarBatalha(Criatura atacante, Criatura defensor, Habilidade habilidade) {
+        if (atacante.estaDerrotado()) {
+            System.out.println(atacante.getNome() + " está derrotado.");
+            return;
+        }
+        if (defensor.estaDerrotado()) {
+            System.out.println(defensor.getNome() + " já está derrotado");
+            return;
+        }
+    }
