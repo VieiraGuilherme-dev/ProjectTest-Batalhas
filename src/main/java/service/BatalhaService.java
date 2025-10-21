edd6d8de909475d83f6017f90315f7b7dd078295
VieiraGuilherme-dev <gv524003@gmail.com>
2025-10-20 21:47:05 -0300

    BatalhaService atualizada com iniciarBatalha pt2

--- src/main/java/service/BatalhaService.java
+++ src/main/java/service/BatalhaService.java
@@ -10,4 +10,38 @@ public class BatalhaService {
             System.out.println(defensor.getNome() + " já está derrotado");
             return;
         }
+        double multiplicador = CalculadoraElemental.calcularMultiplicador(habilidade.getTipo(), defensor.getTipo());
+
+        int danoBase = atacante.getAtk() + habilidade.getPoder() - defensor.getDef();
+        if (danoBase < 0) danoBase = 0;
+
+        int danoFinal = (int) Math.round(danoBase * multiplicador);
+        int novoHp = defensor.getHp() - danoFinal;
+        defensor.setHp(novoHp);
+
+        System.out.println(atacante.getNome() + " usou " + habilidade.getNome() + " causando " + danoFinal + " de dano em " + defensor.getNome() + ".");
+
+        if (multiplicador > 1.0) {
+            System.out.println("É muito bom!");
+        } else if (multiplicador < 1.0) {
+            System.out.println("Não é tão bom!");
+        }
+        if (defensor.estaDerrotado()) {
+            System.out.println("Defensor " + defensor.getNome() + " está derrotado!");
+        }
+
+        EfeitoStatus efeito = habilidade.getEfeito();
+        if (efeito != null && efeito != EfeitoStatus.CURADO) {
+            defensor.aplicarEfeito(efeito);
+            System.out.println(defensor.getNome() + " foi atingido por " + efeito);
+        }
+
+        if (efeito == EfeitoStatus.CURADO) {
+            int cura = habilidade.getPoder();
+            int hpCurado = atacante.getHp() + cura;
+            atacante.setHp(hpCurado);
+            System.out.println(atacante.getNome() + " restaurou " + cura + " de HP.");
+        }
+    }
+
     }
