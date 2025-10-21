b368c74d5ef7638e589271f149fb49f7188fa588
VieiraGuilherme-dev <gv524003@gmail.com>
2025-10-21 00:24:54 -0300

    CriaturaTest modificada

--- src/test/java/model/CriaturaTest.java
+++ src/test/java/model/CriaturaTest.java
@@ -1,4 +1,23 @@
 package model;
 
+import model.*;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class CriaturaTest {
+    @Test
+    public void testCriaturaRecebeEDiminuiHp() {
+        List<Habilidade> habilidades = new ArrayList<>();
+        List<EfeitoStatus> efeitos = new ArrayList<>();
+        Criatura c = new Criatura("Fenix", 100, 20, 10, 5, TipoElemental.FOGO, habilidades, efeitos);
+
+        c.setHp(50);
+        assertEquals(50, c.getHp());
+        assertFalse(c.estaDerrotado());
+
+        c.setHp(0);
+        assertTrue(c.estaDerrotado());
+    }
 }
