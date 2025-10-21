ea1a04c9b9f441bd6e6c9cafd00e7040c8eac758
VieiraGuilherme-dev <gv524003@gmail.com>
2025-10-21 00:33:39 -0300

    Atualização class habilidade

--- src/main/java/model/Habilidade.java
+++ src/main/java/model/Habilidade.java
@@ -2,24 +2,33 @@ package model;
 
 public class Habilidade {
     private String nome;
+    private String descricao;
     private int poder;
     private TipoElemental tipo;
+    private EfeitoStatus efeito;
 
-    public Habilidade(String nome, int poder, TipoElemental tipo) {
+    public Habilidade(String nome, String descricao, int poder, TipoElemental tipo, EfeitoStatus efeito) {
+        super();
         this.nome = nome;
+        this.descricao = descricao;
         this.poder = poder;
         this.tipo = tipo;
+        this.efeito = efeito;
     }
 
     public String getNome() {
         return nome;
     }
-
+    public String getDescricao() {
+        return descricao;
+    }
     public int getPoder() {
         return poder;
     }
-
     public TipoElemental getTipo() {
         return tipo;
+    }
+    public EfeitoStatus getEfeito() {
+        return efeito;
     }
 }
