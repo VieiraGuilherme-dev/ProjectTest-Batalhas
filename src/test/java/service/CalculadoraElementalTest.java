a6a7478e96822a66a5809891f724f9e7ed2e11d3
VieiraGuilherme-dev <gv524003@gmail.com>
2025-10-21 00:38:44 -0300

    CalculadoraElementalTest modificada

--- src/test/java/service/CalculadoraElementalTest.java
+++ src/test/java/service/CalculadoraElementalTest.java
@@ -1,4 +1,27 @@
 package service;
 
-public class CalculadoraElementalTest {
+import service.CalculadoraElemental;
+import model.TipoElemental;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculadoraElementalTest {
+
+    @Test
+    void deveRetornarVantagem() {
+        double multiplicador = CalculadoraElemental.calcularMultiplicador(TipoElemental.ÁGUA, TipoElemental.FOGO);
+        assertEquals(2.0, multiplicador, "Vantagem deve ser 2.0x.");
+    }
+
+    @Test
+    void deveRetornarDesvantagem() {
+        double multiplicador = CalculadoraElemental.calcularMultiplicador(TipoElemental.FOGO, TipoElemental.ÁGUA);
+        assertEquals(0.5, multiplicador, "Desvantagem deve ser 0.5x.");
+    }
+
+    @Test
+    void deveRetornarNeutro() {
+        double multiplicador = CalculadoraElemental.calcularMultiplicador(TipoElemental.LUZ, TipoElemental.TREVAS);
+        assertEquals(1.0, multiplicador, "Neutro deve ser 1.0x.");
+    }
 }
